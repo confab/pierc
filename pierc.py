0ba9304807ac48485dce973be3ac56947e7d1d2a
confab <blooough@gmail.com>
2011-08-08 13:20:07 -0700

    basic shell

--- pierc.py
+++ pierc.py
@@ -3,5 +3,37 @@
 
 import Tkinter as tk
 
+class IrcWindow(object):
+    """Main window."""
+
+    def __init__(self, parent):
+        self.parent = parent
+
+        # One frame to rule them all!
+        self.frm_sauron = tk.Frame(self.parent)
+        self.frm_sauron.pack(expand=tk.YES, fill=tk.BOTH)
+
+        # Create frame to deal with server messages.
+        self.frm_server = tk.Frame(self.frm_sauron, bg='red') # remove bg color
+        self.frm_server.pack(side=tk.TOP, expand=tk.YES, fill=tk.BOTH)
+
+        # Create frame to deal with client messages.
+        self.frm_client = tk.Frame(self.frm_sauron, bg='blue') # remove bg color
+        self.frm_client.pack(side=tk.BOTTOM, fill=tk.X)
+
+        # Create message widget to display user names.
+        self.msg_user = tk.Message(self.frm_server, bg='tan') # remove bg color
+        self.msg_user.pack(side=tk.LEFT, fill=tk.Y)
+
+        # Create message widget to display chat messages.
+        self.msg_chat = tk.Message(self.frm_server, bg='cyan') # remove bg color
+        self.msg_chat.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.BOTH)
+
+        # Create text widget for chat entry.
+        self.txt_entry = tk.Text(self.frm_client, height=1,
+                                 bg='purple') # remove bg color
+        self.txt_entry.pack(side=tk.BOTTOM, fill=tk.X)
+
 root = tk.Tk()
+irc = IrcWindow(root)
 root.mainloop()
