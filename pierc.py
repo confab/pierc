018b459f2ce0291e24b086f6a44179cd74c978a6
confab <blooough@gmail.com>
2011-08-08 20:16:14 -0700

    server window opens properly

--- pierc.py
+++ pierc.py
@@ -6,10 +6,14 @@ import Tkinter as tk
 class IrcWindow(object):
     """Main window."""
 
-    def __init__(self, parent):
+    def __init__(self, parent, dict_settings):
         
         self.parent = parent
+        self.settings = dict_settings
 
+        # Call the server window.
+        self.on_win_server()
+        
         # One frame to rule them all!
         self.frm_sauron = tk.Frame(self.parent)
         self.frm_sauron.pack(expand=tk.YES, fill=tk.BOTH)
@@ -35,38 +39,54 @@ class IrcWindow(object):
                                  bg='purple') # remove bg color
         self.txt_entry.pack(side=tk.BOTTOM, fill=tk.X)
 
-class ServerWindow(object):
-    """Server window."""
+    def on_win_server(self):
+        """Server window."""
 
-    def __init__(self, parent):
-
-        self.parent = parent
+        # Create toplevel frame.
+        win_server = tk.Toplevel()
 
         # One frame to rull them all!
-        self.frm_sauron = tk.Frame(self.parent)
-        self.frm_sauron.pack(expand=tk.YES, fill=tk.BOTH)
+        frm_sauron = tk.Frame(win_server)
+        frm_sauron.pack(expand=tk.YES, fill=tk.BOTH, ipadx=5, ipady=5)
 
         # Create server label.
-        tk.Label(self.frm_sauron, text='Server').pack(side=tk.TOP, anchor=tk.W)
+        tk.Label(frm_sauron, text='Server').pack(side=tk.TOP, anchor=tk.W)
 
         # Create server text entry.
-        self.txt_server = tk.Text(self.frm_sauron, height=1, width=40)
-        self.txt_server.pack(side=tk.TOP, anchor=tk.W)
+        txt_server = tk.Text(frm_sauron, height=1, width=40)
+        txt_server.focus_force()
+        txt_server.pack(side=tk.TOP, anchor=tk.W)
 
         # Create port label.
-        tk.Label(self.frm_sauron, text='Port').pack(side=tk.TOP, anchor=tk.W)
+        tk.Label(frm_sauron, text='Port').pack(side=tk.TOP, anchor=tk.W)
 
         # Create port text entry.
-        self.txt_server = tk.Text(self.frm_sauron, height=1, width=40)
-        self.txt_server.pack(side=tk.TOP, anchor=tk.W)
+        txt_server = tk.Text(frm_sauron, height=1, width=40)
+        txt_server.pack(side=tk.TOP, anchor=tk.W)
 
+        # Create channel label.
+        tk.Label(frm_sauron, text='Chan.').pack(side=tk.TOP, anchor=tk.W)
 
-# Open the server window first.
-win_server = tk.Tk()
-irc = ServerWindow(win_server)
-win_server.mainloop()
+        # Create channel text entry.
+        txt_channel = tk.Text(frm_sauron, height=1, width=40)
+        txt_channel.pack(side=tk.TOP, anchor=tk.W)
+
+        # Create Cancel button.
+        btn_cancel = tk.Button(frm_sauron, text='Cancel', width=8)
+        btn_cancel.bind('<Button-1>', lambda _ : win_server.destroy())
+        btn_cancel.bind('<Return>', lambda _ : win_server.destroy())
+        btn_cancel.pack(side=tk.RIGHT, anchor=tk.E)
+
+        # Create Connect button.
+        btn_connect = tk.Button(frm_sauron, text='Connect', width=8)
+        btn_connect.bind('<Button-1>', lambda _ : win_server.destroy())
+        btn_connect.bind('<Return>', lambda _ : win_server.destroy())
+        btn_connect.pack(side=tk.RIGHT, anchor=tk.W)
+
+# Create settings object
+dict_settings = {}
 
 # Now open the main window.
 root = tk.Tk()
-irc = IrcWindow(root)
+irc = IrcWindow(root, dict_settings)
 root.mainloop()
